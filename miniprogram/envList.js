e95e296ecba9f3b00392f5e21aa9f0ec8af598f3
Kunhong Chan <ckhckh85@gmail.com>
2021-09-11 23:40:02 +0800

    init project

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"demo-9b4637","alias":"demo"},{"envId":"teachers-17hsr","alias":"teachers"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e demo-9b4637 --n quickstartFunctions --r --project "/Users/kunhongchan/WeChatProjects/Assistant" && "/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e teachers-17hsr --n quickstartFunctions --r --project "/Users/kunhongchan/WeChatProjects/Assistant"
